a6cbbda7174193f21c698b079dc5cb5614604d7b
Ahmed Choudhry <achoudhry@meaa.mea.com>
2024-09-30 11:34:14 -0400

    open3d image display

---
+++ o3d/import open3d as o3d.py
@@ -0,0 +1,16 @@
+import open3d as o3d
+import numpy as np
+
+# Create a simple point cloud
+points = np.random.rand(100, 3)  # 100 random 3D points
+colors = np.random.rand(100, 3)   # Random colors for each point
+
+# Create an Open3D point cloud object
+pcd = o3d.geometry.PointCloud()
+pcd.points = o3d.utility.Vector3dVector(points)
+pcd.colors = o3d.utility.Vector3dVector(colors)
+
+# Visualize the point cloud
+o3d.visualization.draw_geometries([pcd], window_name="Point Cloud", width=800, height=600, left=50, top=50)
+
+#o3d.visualization.draw_geometries([pcd])
